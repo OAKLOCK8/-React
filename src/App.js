6ecd0ed54cd9b01d478cb9a03dba9e1704888042
OAKLOCK8 <115138166+OAKLOCK8@users.noreply.github.com>
2023-03-01 11:23:44 +0700

    Finish

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import Transaction from "./components/Transaction";
+import './App.css';
+import Formcomponent from "./components/Formcomponent";
+import { useState,useEffect } from "react";
+import DataContext from "./data/DataContext";
+import ReportComponent from "./components/ReportComponent";
+import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom"
+
+function App() {
+  const design = {color:"red",textAlign:"center",fontSize:"1.5rem"}
+
+  const[items,setItems] = useState([])
+
+  const[reportIncome,setReportIncome] = useState(0)
+  const[reportExpense,setReportExpense] = useState(0)
+  const onAddNewItem = (newItem)=>{
+    setItems((prevItem)=>{
+      return [newItem,...prevItem]
+    })
+  }
+  useEffect(()=>{
+    const amounts = items.map(items=>items.amount)
+    const income = amounts.filter(element=>element>0).reduce((total,element)=>total+=element,0)
+    const expense = (amounts.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1
+    setReportIncome(income.toFixed(2))
+    setReportExpense(expense.toFixed(2))
+  },[items,reportIncome,reportExpense])
+  return (
+    <DataContext.Provider value={{income : reportIncome,expense : reportExpense}}>
+      <div className="container">
+        <h1 style={design}>แอพบัญชีรายรับ - รายจ่าย</h1>
+        <Router>
+        <div>
+          <ul className="horizontal-menu">
+            <li>
+              <Link to="/">ข้อมูลบัญชี</Link>
+            </li>
+            <li>
+              <Link to="/insert">บันทึกข้อมูล</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" exact element={<ReportComponent/>}/>
+            <Route path="/insert" element={<><Formcomponent onAddItem={onAddNewItem}/><Transaction items = {items}/></>}/>
+          </Routes>
+        </div>
+        </Router>
+      </div>
+    </DataContext.Provider>
+  );
+}
+
+export default App;
